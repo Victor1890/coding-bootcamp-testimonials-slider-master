65f99fab31cd4e47e1e07d1e9da2c6cda018e6e7
Victor1890 <victorrosario.1890@gmail.com>
2020-11-30 10:51:29 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,8 @@ let images = ["../src/img/image-john.jpg", "../src/img/image-john.jpg"];
 
 btn_prev.addEventListener("click", (e) => {
   e.preventDefault();
-  let n = -1;
-  showSliderAndText(n);
+  let n = 1;
+  showSliderAndText((n += 1));
 });
 
 btn_next.addEventListener("click", (e) => {
@@ -22,12 +22,9 @@ btn_next.addEventListener("click", (e) => {
 });
 
 const showSliderAndText = (n) => {
-  if (n >= 0) {
-    window.drawer_1.setAttribute("src", images[0]);
-    window.drawer_2.setAttribute("src", images[1]);
-  } else {
-    window.drawer_1.setAttribute("src", images[n - 1]);
-    window.drawer_2.setAttribute("src", images[n - 1]);
-  }
+  if (n <= images.length || n >= images.length) {
+    window.drawer_1.setAttribute("src", images[n]);
+    window.drawer_2.setAttribute("src", images[n]);
+  } else n = 0;
 };
 // "../img/image-john.jpg"
